58f3d081e1f6f283fe3f3c37e992e81aacd46cfe
Pablo Villalobos <pavr1@hotmail.com>
2024-08-04 11:26:19 -0600

    adding http endpoints

--- main.go
+++ main.go
@@ -5,32 +5,46 @@ import (
 	"net/http"
 
 	"github.com/pavr1/people/config"
+	_http "github.com/pavr1/people/handlers/http"
+	"github.com/pavr1/people/handlers/repo"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	setupLogger()
+	log := setupLogger()
 	config, err := config.NewConfig()
 	if err != nil {
 		return
 	}
 
+	repoHandler, err := repo.NewRepoHandler(log, config)
+	if err != nil {
+		log.WithError(err).Error("Failed to create repo handler")
+
+		return
+	}
+
+	httpHandler := _http.NewHttpHandler(repoHandler)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Info("*")
-		fmt.Fprintf(w, "*")
 	})
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world!")
-	})
+	http.HandleFunc("/person", httpHandler.GetPersonList)
+	http.HandleFunc("/person/create", httpHandler.CreatePerson)
+	http.HandleFunc("/person/update", httpHandler.UpdatePerson)
+	http.HandleFunc("/person/delete", httpHandler.DeletePerson)
+	http.HandleFunc("/person/:id", httpHandler.GetPerson)
 
 	log.WithField("port", config.Server.Port).Info("Starting Server...")
 	// Start the HTTP server
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Server.Port), nil))
 }
 
-func setupLogger() {
+func setupLogger() *log.Entry {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
 	})
+
+	return log.NewEntry(log.StandardLogger())
 }
